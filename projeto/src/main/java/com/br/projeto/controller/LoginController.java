266e5be0beeab03379626be0e9e099fc9ce8dbac
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-06-07 22:29:16 -0300

    Spring Boot: Adicionando Campos + LoginController

--- projeto/src/main/java/com/br/projeto/controller/LoginController.java
+++ projeto/src/main/java/com/br/projeto/controller/LoginController.java
@@ -5,12 +5,41 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.br.projeto.model.IUsuarioRepository;
+import com.br.projeto.model.Usuario;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
 public class LoginController {
 
-	@RequestMapping(path="/login", method=RequestMethod.GET)
-    public String index(Model model) {
+	private final IUsuarioRepository usuarioRepository;
 
-        return "login";
-    }
+	public LoginController(IUsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
+
+	@RequestMapping(path = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login(@ModelAttribute("Usuario") Usuario usuario, Model model) {
+
+		String email = usuario.getEmail();
+		String senha = usuario.getSenha();
+
+		usuario = usuarioRepository.findByEmail(email);
+
+		if (usuario != null && usuario.getSenha().equals(senha)) {
+			return "redirect:/";
+		} else {
+			model.addAttribute("error", "Email ou senha Incorretos");
+			return "login";
+		}
+
+	}
 }
--- projeto/src/main/java/com/br/projeto/model/Usuario.java
+++ projeto/src/main/java/com/br/projeto/model/Usuario.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "usuario")
@@ -24,38 +25,92 @@ public class Usuario {
 	
 	@Column(name = "senha",columnDefinition = "TEXT", nullable = true)
 	private String senha;
+
+	@Column(name = "confirmar_senha", columnDefinition = "TEXT", nullable = true)
+	private String confirmar_senha;
 	
 	@Column(name = "telefone",length = 15, nullable = true)
 	private String telefone;
-	
+
+	@Column(name = "endereco", length = 150, nullable = true)
+	private String endereco;
+
+	@Column(name = "cpf", length = 15, nullable = true)
+	private String cpf;
+
+	@Column(name = "data_nascimento", nullable = true)
+	private Date data_nascimento;
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public String getConfirmar_senha() {
+		return confirmar_senha;
+	}
+
+	public void setConfirmar_senha(String confirmar_senha) {
+		this.confirmar_senha = confirmar_senha;
+	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+	
 }
